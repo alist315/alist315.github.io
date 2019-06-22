122434e58f09ac0a47e65788bc57b8fdac30c810
alist315 <aaron.reinheimner315@gmail.com>
2019-06-22 14:30:55 -0400

    responsive

--- app.js
+++ app.js
@@ -5,3 +5,53 @@ $(window).on("load", function () {
     }
     $(".aaron").on("click", close);
 });
+
+
+$(window).on("scroll", function () {
+    let scrollPosition = $(window).scrollTop();
+    console.log("The distance scrolled is: " + scrollPosition);
+
+    if (scrollPosition > 600) {
+        $(".icons").addClass("float");
+    }
+    else {
+        $(".icons").removeClass("float");
+    }
+});
+
+$(window).on("scroll", function () {
+    let scrollPosition = $(window).scrollTop();
+    console.log("The distance scrolled is: " + scrollPosition);
+
+    if (scrollPosition > 1200) {
+        $(".quote").addClass("float1");
+    }
+    else {
+        $(".quote").removeClass("float1");
+    }
+});
+
+
+$(window).on("scroll", function () {
+    let scrollPosition = $(window).scrollTop();
+    console.log("The distance scrolled is: " + scrollPosition);
+
+    if (scrollPosition > 600) {
+        $("me").addClass("float1");
+    }
+    else {
+        $("me").removeClass("float1");
+    }
+});
+
+$(document).ready(function(){
+  $(".button").click(function(){
+
+      if($(".button").text() == "☰"){
+       $(".button").text("✖");
+      }else{
+        $(".button").text("☰");
+      }
+    $("a").toggle("slow");
+  });
+});
